8ba836c8606169893c26d185b7a4648bf437ed4b
Unknown <jorgenaw@hotmail.com>
2018-12-06 16:10:33 +0100

    Server update

--- AndroidClient/app/src/main/java/com/example/jorge/contactmanager/HTTPhandler.java
+++ AndroidClient/app/src/main/java/com/example/jorge/contactmanager/HTTPhandler.java
@@ -1,15 +1,25 @@
 package com.example.jorge.contactmanager;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+
+
+
 
 public final class HTTPhandler {
 
@@ -17,7 +27,7 @@ public final class HTTPhandler {
 
     static {
         try {
-            url = new URL("127.0.0.1");
+            url = new URL("http://192.168.1.139:8080/ContactManagerServer/webresources/com.mycompany.contactmanagerserver.contact");
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
@@ -42,21 +52,42 @@ public final class HTTPhandler {
     }
 
     public static void addContact(Contact contact) throws IOException {
-        con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("PUT");
-        Map<String, String> parameters = new HashMap<>();
-        parameters.put("param1", contact.getFirstName());
-        parameters.put("param2", contact.getLastName());
-        parameters.put("param3", contact.getPhoneNumber());
-        parameters.put("param4", contact.getEmail());
-        parameters.put("param5", contact.getAddress());
+        contact.setId(10);
 
-        con.setDoOutput(true);
-        DataOutputStream out = new DataOutputStream(con.getOutputStream());
-        out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
-        out.flush();
-        out.close();
-        con.disconnect();
+         Gson gson = new Gson();
+
+
+                try {
+
+
+                    String json = gson.toJson(contact);
+                    String jsonEncoded = URLEncoder.encode(json, "UTF-8");
+                    int jsonLength = json.length();
+
+
+                    con = (HttpURLConnection) url.openConnection();
+                    //con.connect();
+                    con.setDoOutput(true);
+                    con.setInstanceFollowRedirects(false);
+                    con.setRequestMethod("POST");
+                    con.setRequestProperty("charset", "utf-8");
+                    con.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                    con.setRequestProperty("Accept", "application/json");
+                    con.setRequestProperty("Content-Length", Integer.toString(jsonLength));
+                    con.setDoOutput(true);
+                    con.setDoInput(true);
+
+
+                    DataOutputStream os = new DataOutputStream(con.getOutputStream());
+                    os.writeBytes(json);
+                    os.flush();
+                    os.close();
+                    con.disconnect();
+                } catch (Exception e) {
+
+
+                }
+
 
     }
 
